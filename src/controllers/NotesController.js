e89d6be89020512ea1c777eb3dfc7e0bf074f729
LABDesenvolver <gui_wb@hotmail.com>
2023-01-31 17:32:02 -0300

    update NotesController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -61,7 +61,7 @@ class NotesController {
         "movie_notes.user_id",
       ])
       .where("movie_notes.user_id", user_id)
-      .whereLike("movie_notes.title", `%${title}%`)
+      /* .whereLike("movie_notes.title", `%${title}%`) */
       .whereIn("name", filterTags)
       .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id") //tabela, campos da tabela que tem em comum
       .orderBy("movie_notes.title")
@@ -69,13 +69,13 @@ class NotesController {
   } else {
     notes = await knex("movie_notes")
     .where({ user_id })
-    .whereLike("title", `%${title}%`)
-    .orderBy("notes.title");
+    /* .whereLike("title", `%${title}%`) */
+    .orderBy("movie_notes.title");
   }
 
   const userTags = await knex("movie_tags").where({ user_id });
   const notesWithTags = notes.map(note => {
-    const noteTags = userTags.filter(tag => tag.note_id === note.id);
+  const noteTags = userTags.filter(tag => tag.note_id === note.id);
 
     return{
       ...note,
@@ -84,7 +84,8 @@ class NotesController {
   })
  
   return response.json(notesWithTags);
- }
+ } 
+
 }
 
 module.exports = NotesController;
